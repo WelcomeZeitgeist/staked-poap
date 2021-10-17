55e56310020ac87c4ed60c33e2264be636a8c30a
Andy <andy@trulymine.ink>
2021-10-17 17:22:15 +0200

    Inserted sample files, compiled, tested. todo: deploy

---
+++ truffle_test/contracts/SimpleStorage.sol
@@ -0,0 +1,15 @@
+
+   
+pragma solidity ^0.5.16;
+
+contract SimpleStorage {
+  uint data;
+
+  function updateData(uint _data) external {
+    data = _data;
+  }
+
+  function readData() external view returns(uint) {
+    return data;
+  }
+}
